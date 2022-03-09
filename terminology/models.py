25c3bb4ee077d9966e708b9e89a1eef284ab96a3
kiriharu <kiriharu@yandex.ru>
2022-03-09 21:58:38 +0300

    Модели

--- terminology/models.py
+++ terminology/models.py
@@ -1,3 +1,92 @@
 from django.db import models
 
-# Create your models here.
+
+class Directory(models.Model):
+    """ Модель справочника """
+    name = models.CharField(
+        verbose_name="Наименование",
+        max_length=255,
+        unique=True,
+    )
+    short_name = models.CharField(
+        verbose_name="Короткое наименование",
+        max_length=128,
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        blank=True,
+        null=False,
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Справочник"
+        verbose_name_plural = "Справочники"
+
+
+class DirectoryVersion(models.Model):
+    """ Модель версии справочника """
+    directory = models.ForeignKey(
+        "Directory",
+        verbose_name="Справочник",
+        on_delete=models.CASCADE,
+        related_name="versions",
+    )
+    version = models.CharField(
+        verbose_name="Версия",
+        max_length=255,
+    )
+    start_date = models.DateTimeField(
+        verbose_name="Дата начала действия справочника этой версии",
+    )
+
+    def __str__(self):
+        return f"{self.directory.name} : {self.version}"
+
+    class Meta:
+        verbose_name = "Версия справочника"
+        verbose_name_plural = "Версии справочника"
+        constraints = [
+            # уникальная версия справочника
+            models.UniqueConstraint(
+                fields=("directory", "version"),
+                name="unique_directory_version"
+            )
+        ]
+
+
+class Item(models.Model):
+    """ Модель элемента справочника """
+    parent = models.ForeignKey(
+        "self",
+        verbose_name="Родительский идентификатор",
+        on_delete=models.CASCADE,
+        related_name="childs",
+        null=True,
+        blank=True,
+    )
+    code = models.CharField(
+        verbose_name="Код элемента",
+        max_length=256,
+        null=False,
+    )
+    value = models.CharField(
+        verbose_name="Значение элемента",
+        max_length=256,
+        null=False
+    )
+    directory_version = models.ForeignKey(
+        "DirectoryVersion",
+        verbose_name="Версия справочника",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = "Элемент справочника"
+        verbose_name_plural = "Элементы справочника"
+
